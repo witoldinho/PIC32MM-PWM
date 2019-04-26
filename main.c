7596f540fb43e06807932c5a19d63c7d8d167773
wtk <wtk@WTK>
2019-04-26 13:07:58 +0200

    pic32mm0256 GPM 48 PWM i UART1

---
+++ main.c
@@ -0,0 +1,22 @@
+
+#include "mcc_generated_files/system.h"
+#include <stdio.h>
+
+/*
+                         Main application
+ */
+int main(void)
+{
+    // initialize the device
+    SYSTEM_Initialize();
+    printf("\n\r program test PWM \n\r");
+    while (1)
+    {
+        // Add your application code
+    }
+    return 1; 
+}
+/**
+ End of File
+*/
+
